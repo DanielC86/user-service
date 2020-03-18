7f71eb63df73843818775644c3ee534d991d50c4
Daniel Czajka <daniel.czajka@gmail.com>
2020-03-18 22:06:11 +0100

    working on config server-client functionality

--- src/main/java/com/daniel/czajka/userservice/controller/UserController.java
+++ src/main/java/com/daniel/czajka/userservice/controller/UserController.java
@@ -2,6 +2,7 @@ package com.daniel.czajka.userservice.controller;
 
 import com.daniel.czajka.userservice.entity.Messages;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,6 +11,7 @@ import org.springframework.web.client.RestTemplate;
 import java.util.Arrays;
 import java.util.List;
 
+@RefreshScope
 @RestController
 @RequestMapping("/user-app")
 public class UserController {
